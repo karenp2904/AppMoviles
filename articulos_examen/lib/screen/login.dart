37fdf53b15088e942b8871a8456aa378f6286f96
Karen Yulieth Perez Luna <karen2005yulieth@gmail.com>
2025-03-03 10:15:35 -0500

    inicio examen

---
+++ articulos_examen/lib/screen/login.dart
@@ -0,0 +1,83 @@
+import 'package:articulos_examen/services/auth_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class LoginScreen extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+
+      return Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                "Bienvenido",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.pinkAccent,
+                ),
+              ),
+              const SizedBox(height: 30),
+              TextField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Usuario',
+                  prefixIcon: const Icon(Icons.person, color: Colors.pinkAccent),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.pinkAccent),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  prefixIcon: const Icon(Icons.lock, color: Colors.pinkAccent),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.pinkAccent),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              authProvider.isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : ElevatedButton(
+                      onPressed: () {
+                        authProvider.login(
+                          usernameController.text,
+                          passwordController.text,
+                          context,
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pinkAccent,
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: const Text(
+                        "Iniciar Sesión",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
+            ],
+          ),
+        ),
+      );
+    }
+}
